3fee6a2eaf975e0b2700ee1ba0bdd7c03dcc6dd3
Arthur A. <arthurakilalves@gmail.com>
2025-11-22 01:59:22 -0300

    fix: error de autenticação, faltou adicionar no instaledapps

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -41,6 +41,15 @@ INSTALLED_APPS = [
     'chat'
 ]
 
+INSTALLED_APPS += ["rest_framework.authtoken"]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
