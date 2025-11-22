1f7c0a7f563a66ffa04032def99529aa4113dfe5
Arthur A. <arthurakilalves@gmail.com>
2025-11-22 01:56:56 -0300

    feat: criação dos serializers

---
+++ backend/chat/api/v1/serializers.py
@@ -0,0 +1,64 @@
+from rest_framework import serializers
+from rest_framework.exceptions import PermissionDenied
+
+from django.contrib.auth.models import User
+from chat.models import Conversa, Mensagem
+
+
+class UsuarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields =["id", "username"]
+
+class MensagemSerializer(serializers.ModelSerializer):
+    enviado_por = UsuarioSerializer(read_only=True)
+    conversa = serializers.PrimaryKeyRelatedField(queryset=Conversa.objects.all())
+    imagem_url = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Mensagem
+        fields = ['id', 'conversa', 'enviado_por', 'conteudo', 'imagem', 'imagem_url', 'data_envio','bot']
+        read_only_fields = ["id","enviado_por", "data_envio", "bot"]
+
+    def create(self, validated_data):
+        request = self.context.get("request")
+        user = request.user
+        conversa = validated_data["conversa"]
+
+        if conversa.usuario != user:
+            raise PermissionDenied("Você não tem acesso a esta conversa.")
+        
+        mensagem = Mensagem.objects.create(
+            enviado_por=user,
+            bot=False,
+            **validated_data
+        )
+
+        total = conversa.mensagens.count()
+        if total == 1:
+            Mensagem.objects.create(
+                conversa=conversa,
+                enviado_por=user,
+                conteudo=f"Obrigado por entrar em contato {user.username}, já já retornaremos!",
+                bot=True
+            )
+        return mensagem
+
+    def get_imagem_url(self, obj):
+        # transformar a url fornecida pelo DRF como um link pronto para o front consumir
+        request = self.context.get("request")
+        if obj.imagem:
+            if request is not None: 
+                return request.build_absolute_uri(obj.imagem.url) # retorna a url já pronta
+            return obj.imagem.url # envia o caminho relativo
+        return None #não houve imagem retorna nada
+    
+
+class ConversaSerializer(serializers.ModelSerializer):
+    usuario = UsuarioSerializer(read_only=True)
+    mensagens = MensagemSerializer(many=True, read_only=True)
+
+    class Meta: 
+        model = Conversa
+        fields = ['id', 'usuario', 'titulo', 'fechada', 'criada_em', 'mensagens']
+        read_only_fields = ["id", "usuario", "criada_em", "mensagens"]
